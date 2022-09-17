bad9f3006d5b8d66b121397f3ec18ad9492c8431
Mohammedshuhaib <mohammedshuhaib100@gmail.com>
2022-09-17 15:27:52 +0530

    eoor solving

--- controller/auth.js
+++ controller/auth.js
@@ -18,10 +18,26 @@ module.exports = {
     await nodemailer
       .sendOtp(email, name)
       .then(async (otp) => {
-        res.cookie("otpCode", otp);
-        res.cookie("name", name);
-        res.cookie("email", email);
-        res.cookie("password", saltedPassword);
+        res.cookie("otpCode", otp, {
+          sameSite: "none",
+          secure: true,
+          httpOnly: true,
+        });
+        res.cookie("name", name, {
+          sameSite: "none",
+          secure: true,
+          httpOnly: true,
+        });
+        res.cookie("email", email, {
+          sameSite: "none",
+          secure: true,
+          httpOnly: true,
+        });
+        res.cookie("password", saltedPassword, {
+          sameSite: "none",
+          secure: true,
+          httpOnly: true,
+        });
         return res.sendStatus(200).json("success");
       })
       .catch((err) => {
@@ -35,19 +51,19 @@ module.exports = {
 
   SubmitOtp: asyncHandler(async (req, res, next) => {
     let { otpCode, name, email, password } = req.cookies;
-    console.log(req.cookies)
+    console.log(req.cookies);
     let { otp } = req.body;
     if (otp === otpCode) {
-      console.log('ok')
+      console.log("ok");
       try {
         await user.create({ name, email, password });
         return res.status(200).json("otp verification success");
       } catch (err) {
-        console.log(err)
+        console.log(err);
         if (err.code === 11000) {
           return next(createError(409, "User already exist"));
         } else {
-          return next(createError(500, 'Something went wrong'))
+          return next(createError(500, "Something went wrong"));
         }
       }
     } else {
@@ -60,11 +76,11 @@ module.exports = {
     if (!email || !password) return next(createError(400, "Bad request"));
     let User = await user.findOne({ email });
     if (User) {
-      let response =await bcrypt.compare(password, User.password);
-      console.log(response)
+      let response = await bcrypt.compare(password, User.password);
+      console.log(response);
       if (response) {
         const users = { name: User.name };
-        const accessToken = generateAccessToken(users); 
+        const accessToken = generateAccessToken(users);
         const refreshToken = jwt.sign(users, process.env.REFRESH_TOKEN_SECRET);
         await user.updateOne({ _id: User._id }, { $set: { refreshToken } });
         res.cookie("accessToken", accessToken, {
@@ -73,7 +89,7 @@ module.exports = {
         });
         res.cookie("refreshToken", refreshToken);
         res.cookie("userId", User._id);
-        res.sendStatus(200).json(User.name)
+        res.sendStatus(200).json(User.name);
       } else {
         return next(createError(401, "Incorrect password"));
       }
@@ -82,13 +98,16 @@ module.exports = {
     }
   }),
 
-  logout: asyncHandler(async(req,res,next) => {
-    const {userId} = req.cookies
+  logout: asyncHandler(async (req, res, next) => {
+    const { userId } = req.cookies;
     try {
-    let response = await user.updateOne({userId},{$unset:{refreshToken:""}})
-    return res.sendStatus(200).json('ok')
-    }catch(err) {
-      return next(createError(500, 'something went wrong'))
+      let response = await user.updateOne(
+        { userId },
+        { $unset: { refreshToken: "" } }
+      );
+      return res.sendStatus(200).json("ok");
+    } catch (err) {
+      return next(createError(500, "something went wrong"));
     }
-  })
+  }),
 };
